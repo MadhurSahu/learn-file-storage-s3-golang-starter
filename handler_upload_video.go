c3703545bc9de4f16a1c716970358bf3e2396a81
Madhur Sahu <madhursahu@protonmail.com>
2025-09-20 03:22:05 +0530

    [A] presigned URL generation for video retrieval and upload handlers

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -134,7 +134,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, videoKeyName)
+	videoURL := fmt.Sprintf("%s,%s", cfg.s3Bucket, videoKeyName)
 	video.VideoURL = &videoURL
 
 	err = cfg.db.UpdateVideo(video)
@@ -143,7 +143,13 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, video)
+	signedVideo, err := cfg.dbVideoToSignedVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't generate presigned URL", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, signedVideo)
 }
 
 func getVideoOrientation(filePath string) (string, error) {
